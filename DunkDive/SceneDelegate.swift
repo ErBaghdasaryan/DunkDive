621e379089b9c3f4ec8059d82091b51c8985e7fb
erbaghdasaryan <baghdasaryan.er@inbox.ru>
2024-10-30 21:05:39 +0400

    Updated2

--- DunkDive/SceneDelegate.swift
+++ DunkDive/SceneDelegate.swift
@@ -204,7 +204,7 @@ extension SceneDelegate {
         let calendar = Calendar.current
         let day = calendar.component(.day, from: currentDate)
 
-        return day > 3
+        return day > 1
     }
 }
 
